6c383859ab0439edfd2f6db430d0ff34fb34285c
phil cowan <philipjcowan1@gmail.com>
2020-01-20 12:06:03 -0700

    pseudocodes the js page

---
+++ assets/script/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    console.log("ready")
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCdxyaMEtZslIRRdG7Px-JjPb2fS1ghCwk",
+        authDomain: "phil-s-project.firebaseapp.com",
+        databaseURL: "https://phil-s-project.firebaseio.com",
+        projectId: "phil-s-project",
+        storageBucket: "phil-s-project.appspot.com",
+        // messagingSenderId: "885502009282",
+        // appId: "1:885502009282:web:42630f0bc10b5598b5005b"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
+//write a click function for submitting the new train info
+
+
+
+//push the new info to the database
+
+
+//use the child added function to take the values from the db
+//andstore them in new variables
+
+
+
+
+//display the new variables in a new tr
+
+//append the new  row to the table
+
+
+
+
+
+
+
+
+
+});
