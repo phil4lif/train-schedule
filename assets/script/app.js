cc1d70a3d3560517953d78d1cd3c3d2e8981be48
phil cowan <philipjcowan1@gmail.com>
2020-01-22 11:38:44 -0700

    adds authentication

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {
     console.log("ready")
+   
+ // FirebaseUI config.
+//  var uiConfig = {
+//     signInSuccessUrl: '<url-to-redirect-to-on-success>',
+//     signInOptions: [
+//       // Leave the lines as is for the providers you want to offer your users.
 
+//       firebase.auth.GithubAuthProvider.PROVIDER_ID,
+
+//     ],
+//     // tosUrl and privacyPolicyUrl accept either url string or a callback
+//     // function.
+//     // Terms of service url/callback.
+//     tosUrl: '<your-tos-url>',
+//     // Privacy policy url/callback.
+//     privacyPolicyUrl: function() {
+//       window.location.assign('<your-privacy-policy-url>');
+//     }
+//   };
+
+  // Initialize the FirebaseUI Widget using Firebase.
+//   var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+//   ui.start('#firebaseui-auth-container', uiConfig);
     // Your web app's Firebase configuration
     var firebaseConfig = {
         apiKey: "AIzaSyCdxyaMEtZs" + "lIRRdG7Px-JjPb2fS1ghCwk",
